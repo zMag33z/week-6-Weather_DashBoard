78ae0a3a644cec9b99d914613c54e83231a98b2d
zMag33z <mag33.cdh@gmail.com>
2022-11-29 13:31:22 -0600

    weather bg images

---
+++ script.js
@@ -0,0 +1,50 @@
+/*Openweathermap.org*/
+
+
+const WeatherKey = '002e02371bc693eda7b161253481e4e0';
+
+var citySelection;
+
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySelection + "&appid=" + WeatherKey;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+
