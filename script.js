f2a0e874d7ec89176737a9f8992f9103b5a7c3aa
zMag33z <mag33.cdh@gmail.com>
2022-12-04 17:51:33 -0600

    menu opening and closing

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 /*Openweathermap.org*/
 
+/* target variables*/
+const selectionBox = document.getElementById('Selection');
+const toggleMenu = document.getElementById('myButton-OpenClose');
+
+
+/*Click event Open City Selection Window*/
+toggleMenu.addEventListener('click', function(){
+    const openMenu = selectionBox.getAttribute('data-visible');
+    toggleMenu.getAttribute('aria-expanded');
+    if(openMenu === 'false'){
+        selectionBox.setAttribute('data-visible', true);
+        toggleMenu.setAttribute('aria-expanded', true);
+        selectionBox.setAttribute('aria-expanded', true);
+    }else if(openMenu === 'true'){
+        selectionBox.setAttribute('data-visible', false);
+        toggleMenu.setAttribute('aria-expanded', false);
+        selectionBox.setAttribute('aria-expanded', false);
+    }
+})
 
 
 
 
-var citySelection;
+// var citySelection;
 
 
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySelection + "&appid=" + WeatherKey;
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySelection + "&appid=" + WeatherKey;
 
 
 
