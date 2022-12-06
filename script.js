deba18e55787dfbd8df6d5adad65cf519285aebf
zMag33z <mag33.cdh@gmail.com>
2022-12-05 20:32:35 -0600

    almost to fetch

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
+/*What I'm working with*/
+/*Provided in Challenge READme*/
 /*https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys*/
+
 /*Openweathermap.org*/
+/*Geocoding API:   http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}*/
+/*5Day/3Hour forecast API:  api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}*/
+/*Current Weather API:  https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}*/
+//ISO 3166-2:US
 
 
 /* target variables*/
 const selectionBox = document.getElementById('Selection');
 const toggleMenu = document.getElementById('myButton-OpenClose');
+const citySubmit = document.getElementById('user-Selected');
+const cityInput = document.querySelector('#city-Input');
+const clearStorage = document.getElementById('clear');
 
 
 /*Click event Open City Selection Window*/
-toggleMenu.addEventListener('click', function(){
+toggleMenu.addEventListener('click', function set(){
+    closeMenu();
+});
+
+/*Click event clear City localstorage*/
+citySubmit.addEventListener('submit', function(e) {
+    e.preventDefault();
+    closeMenu();
+/*If localstorage empty create object holder*/
+    if(localStorage.getItem('cities') == null){
+        localStorage.setItem('cities', `[]`);
+    }
+/*Pull localstorage*/
+    let previousCities = JSON.parse(localStorage.getItem('cities'));
+    let city = cityInput.value;
+    let sameCity = city;
+/*Filter storage for any repeats*/
+    function removeMatchingCity(arr, value){
+        return arr.filter(function(element){
+            return element != value;  /*if not equal to a repeat return to array*/
+        })
+    }
+
+    let noMatchingCities = removeMatchingCity(previousCities, sameCity);
+    noMatchingCities.unshift(city);
+    localStorage.setItem('cities', JSON.stringify(noMatchingCities));
+});
+
+/*Clear cities from localstorage.*/
+clearStorage.addEventListener('click', function(){
+    localStorage.removeItem('cities');
+});
+
+/*Function is two way. Opens and closes menu while changing attribute values*/
+/*Run by two eventlisteners- toggleMenu and citySubmit*/
+function closeMenu(){
     const openMenu = selectionBox.getAttribute('data-visible');
     toggleMenu.getAttribute('aria-expanded');
     if(openMenu === 'false'){
@@ -20,71 +65,14 @@ toggleMenu.addEventListener('click', function(){
         toggleMenu.setAttribute('aria-expanded', false);
         selectionBox.setAttribute('aria-expanded', false);
     }
-});
+}
 
-const citySubmit = document.getElementById('city-btn');
-const cityInput = document.querySelector('#city-Input').value;
+let citySearch = JSON.parse(localStorage.getItem('cities'));
+let myKey = '&appid=002e02371bc693eda7b161253481e4e0';
+let geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?';
+let cityCountry = 'q=' + citySearch[0] + ',US';
+let limitCity = '&limit=1';
 
-citySubmit.addEventListener('submit', function(event) {
-    console.log("YOU CLICKED IT!");
-    event.preventDefault();
-
-    // if(localStorage.getItem('cities') == null){
-    //     localStorage.setItem('cities', `[]`);
-    // }
-
-    // let city = cityInput;
-
-    // let previousCities = JSON.parse(localStorage.getItem('cities'));
-    // previousCities.push(city);
-    // localStorage.setItem('cities', JSON.stringify(previousCities));
-});
-
-// let citySearch = json.parse(localStorage.getItem('cities'[0]));
-// let myKey = 'appid=002e02371bc693eda7b161253481e4e0';
-// let geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?';
-// let cityCountry = 'q=' + citySearch + ',US';
-// let limitCity = '&limit=5';
-// let cityFile = geoUrl + cityCountry + limitCity + myKey;
-
-
-//city state lon lat http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-//weather http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//geoloc http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-//ISO 3166-2:US
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let getCoords = geoUrl + cityCountry + limitCity + myKey;
 
 
